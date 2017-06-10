98416d191e1d257cb7c3829772989696a3cca70a
Andrzej Longosz <andrzej@longosz.net>
2017-06-10 13:26:18 +0200

    Bound PhoneBook view data to model

--- src/net/longosz/PhoneBook/Controller.java
+++ src/net/longosz/PhoneBook/Controller.java
@@ -1,10 +1,37 @@
 package net.longosz.PhoneBook;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import net.longosz.PhoneBook.model.Person;
 
 public class Controller {
 
     @FXML
-    private TableView<?> phoneList;
+    private TextField name;
+
+    @FXML
+    private TextField phone;
+
+    @FXML
+    private TableView<Person> phoneListView;
+
+    @FXML
+    private TableColumn<Person, String> nameColumn;
+
+    @FXML
+    private TableColumn<Person, String> phoneColumn;
+
+    private final ObservableList<Person> phoneList = FXCollections.observableArrayList();
+
+    @FXML
+    public void initialize() {
+        phoneListView.setItems(phoneList);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+    }
 }
